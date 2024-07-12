0c79bb76509a69ba9ed0ce9f9a2cad48c1ac92c2
twwu123 <tszwaiwu.96@gmail.com>
2024-07-12 09:57:27 +0800

    add minting example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import { BlockfrostProvider, MeshTxBuilder } from "@meshsdk/core";
+import {
+  BlockfrostProvider,
+  MeshTxBuilder,
+  NativeScript,
+  resolveNativeScriptHash,
+  resolveNativeScriptHex,
+  resolvePaymentKeyHash,
+} from "@meshsdk/core";
 import { BrowserWallet } from "@meshsdk/core";
 import { useEffect, useState } from "react";
 
@@ -70,7 +77,7 @@ const Home: NextPage = () => {
       ])
       .changeAddress(changeAddress)
       .complete();
-  
+
     // Complete the signing process in the browser wallet
     try {
       const signedTx = await wallet.signTx(txBuilder.txHex);
@@ -84,8 +91,8 @@ const Home: NextPage = () => {
   // the above transaction will log a signed transaction hex. It should look something like
   // 84a50081825820e63e499549d8... This is called a CBOR hex, and it will be something we
   // must get very familiar with. Once you get this CBOR hex, I want you to decode it into something
-  // human readable using https://cbor.me/. 
-  
+  // human readable using https://cbor.me/.
+
   // The second thing I would like you to do is to find the open source repository where Cardano's
   // cddl files are stored (hint: IntersectMBO). Look through the repository to find the current active
   // cddl (the current era is named babbage).
@@ -99,6 +106,83 @@ const Home: NextPage = () => {
   // outputs:
   // fee:
 
+  const mintExample = async () => {
+    if (!wallet) {
+      alert("Please connect your wallet");
+      return;
+    }
+    if (!process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY) {
+      alert("Please set up environment variables");
+      return;
+    }
+
+    // Set up tx builder with blockfrost support
+    const blockfrost: BlockfrostProvider = new BlockfrostProvider(
+      process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY,
+      0
+    );
+    const txBuilder: MeshTxBuilder = new MeshTxBuilder({
+      fetcher: blockfrost,
+      submitter: blockfrost,
+    });
+
+    // Get utxos from wallet, with selection
+    const inputUtxos = await wallet?.getUtxos([
+      {
+        unit: "lovelace",
+        quantity: "5000000",
+      },
+    ]);
+
+    if (!inputUtxos) {
+      alert(
+        "Utxo selection failed, either wallet has insufficient funds, or something went wrong"
+      );
+      return;
+    }
+
+    // Add all selected inputs into the tx builder
+    for (let i = 0; i < inputUtxos.length; i++) {
+      const utxo = inputUtxos[i];
+      txBuilder.txIn(utxo.input.txHash, utxo.input.outputIndex);
+    }
+
+    // Get change address from wallet
+    const changeAddress = await wallet.getChangeAddress();
+
+    // This is a very simple native script that defines the
+    // minting conditions of our token
+    const nativeScript: NativeScript = {
+      type: "sig",
+      keyHash: resolvePaymentKeyHash(changeAddress),
+    };
+
+    // Send the minted token with 2 ADA back to our change address
+    await txBuilder
+      .txOut(changeAddress, [
+        {
+          unit: "lovelace",
+          quantity: "2000000",
+        },
+      ])
+      .mint(
+        "1",
+        resolveNativeScriptHash(nativeScript),
+        Buffer.from("TEST", "utf-8").toString("hex") // Note that asset names are base16 encoded, so we cannot just input "TEST"
+      )
+      .mintingScript(resolveNativeScriptHex(nativeScript))
+      .changeAddress(changeAddress)
+      .complete();
+
+    // Complete the signing process in the browser wallet
+    try {
+      const signedTx = await wallet.signTx(txBuilder.txHex);
+      console.log(signedTx);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       {wallet ? (
@@ -110,6 +194,9 @@ const Home: NextPage = () => {
       )}
       <button className={styles.button} onClick={sendExample}>
         Send
+      </button>
+      <button className={styles.button} onClick={mintExample}>
+        Mint Token
       </button>
     </div>
   );
